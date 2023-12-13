e312bdc4f637abf2810148d06749f941f53fc642
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-12-13 16:15:19 +0200

    user routes and post routes

--- server/index.js
+++ server/index.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const {connect} = require('mongoose');
 require('dotenv').config();
 
+const userRoutes = require('./routes/userRoutes')
+const postRoutes = require('./routes/postRoutes')
+
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
 app.use(cors({credentials: true, origin: "http://localhost:3000"}))
+
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
 
 
 
