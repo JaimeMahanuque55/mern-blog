b91eed176d96cb22e76c83fa50055af3906ef359
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-12-14 15:28:58 +0200

    get authors function

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -112,7 +112,12 @@ const editUser = async (req, res, next) => {
 // GET : api/users/authors
 // UNPROTECTED
 const getAuthors = async (req, res, next) => {
-    res.json("Get all users/authors")
+    try {
+        const authors = await User.find().select('-password');
+        res.json(authors)
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 module.exports = {registerUser, loginUser, getUser, changeAvatar, editUser, getAuthors}
