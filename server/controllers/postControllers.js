4ad27463531a9d36b9f13df4b5c82334997d0d6b
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-12-15 22:05:49 +0200

    Post Controllers

---
+++ server/controllers/postControllers.js
@@ -0,0 +1,51 @@
+
+
+// ============================ CREATE A POST
+// POST : api/posts
+// PROTECTED
+const createPost = async (req, res, next) => {
+    res.json("Create post")
+}
+
+
+// ============================ GET ALL POSTS
+// GET : api/posts
+// UNPROTECTED
+const getPosts = async (req, res, next) => {
+    res.json("Get all posts")
+}
+
+// ============================ GET SINGLE POST
+// GET : api/posts/:id
+// UNPROTECTED
+const getPost = async (req, res, next) => {
+    res.json("GET single post")
+}
+
+// ============================ GET POSTS BY CATEGORY
+// GET : api/posts/categories/:category
+// UNPROTECTED
+const getCatPosts = async (req, res, next) => {
+    res.json("Get posts by category")
+}
+
+// ============================ GET AUTHOR POST
+// GET : api/posts/users/:id
+// UNPROTECTED
+const getUserPosts = async (req, res, next) => {
+    res.json("Get user posts")
+}
+
+// ============================ EDIT POST
+// PATCH : api/posts/:id
+// PROTECTED
+const editPost = async (req, res, next) => {
+    res.json("Edit post")
+}
+
+// ============================ DELETE POST
+// DELETE : api/posts/:id
+// PROTECTED
+const deletePost = async (req, res, next) => {
+    res.json("Delete post")
+}
