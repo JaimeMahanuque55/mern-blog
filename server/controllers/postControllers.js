9a8fca926465b1da575cc92dfdbdbb8b21364b0b
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-12-16 14:07:10 +0200

    Create Post function

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,10 +1,46 @@
-
+const Post = require('../models/postModel');
+const User = require("../models/userModel");
+const path = require("path");
+const fs = require("fs");
+const {v4: uuid} = require("uuid");
+const HttpError = require("../models/errorModel");
 
 // ============================ CREATE A POST
 // POST : api/posts
 // PROTECTED
 const createPost = async (req, res, next) => {
-    res.json("Create post")
+    try {
+        let {title, category, description} = req.body;
+        if(!title || !category || !description || !req.files) {
+            return next(new HttpError("Fill in all fields  and choose thumbnail.", 422))
+        }
+        const {thumbnail} = req.files;
+        // check the file size
+        if(thumbnail.size > 2000000) {
+            return next(new HttpError("Thumbnail too big. File should be less than 2mb."))
+        }
+        let fileName = thumbnail.name;
+        let splittedFileName = fileName.split('.');
+        let newFileName = splittedFileName[0] + uuid() + "." + splittedFileName[splittedFileName.length - 1];
+        thumbnail.mv(path.join(__dirname, '..', '/uploads', newFileName), async (err) => {
+            if(err) {
+                return next(new HttpError(err))
+            } else {
+                const newPost = await Post.create({title, category, description, thumbnail: newFileName, creator: req.user.id});
+                if(!newPost) {
+                    return next(new HttpError("Post couldn't be created.", 422));
+                }
+                // find user and increase post count by 1
+                const currentUser = await User.findById(req.user.id);
+                const userPostCount = currentUser.posts + 1;
+                await User.findByIdAndUpdate(req.user.id, {posts: userPostCount});
+
+                res.status(201).json(newPost);
+            }
+        })
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
